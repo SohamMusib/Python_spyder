4ac8d06d2858c176bb6ff7a66af74eb64152238f
tanmoyee04 <61410535+tanmoyee04@users.noreply.github.com>
2020-10-04 14:46:44 +0530

    Updated

--- Pokemon_api.py
+++ Pokemon_api.py
@@ -1,12 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 29 16:42:32 2020
-
-@author: Tanmoyee
-"""
-
 import requests
-import json
+import json #This can be omited
 import matplotlib.pyplot as plt
 
 def get_pokemone_data(name="raichu"):
@@ -19,7 +12,9 @@ pokemone_name=input("enter the name of the pokemone :")
 recieved_data=get_pokemone_data(pokemone_name)
 #print(recieved_data['cards'])
 url_data=requests.get(recieved_data["cards"][1]["imageUrl"])
-
+with open('./001.png','wb')as f:
+    for item in url_data.iter_content(1024):
+        f.write(item)
 image_data=plt.imread('./001.png')
 plt.imshow(image_data)
 plt.show()
